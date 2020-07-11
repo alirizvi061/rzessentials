a8ce5f4bc5a7c0d9e2d35b7cdddc82a4c8a14917
Ali Rizvi <alirizvi@Starboys-Mac.local>
2020-07-11 12:32:49 -0700

    trying to work on the redirect

--- server.js
+++ server.js
@@ -73,8 +73,14 @@ app.use('/sessions', sessionsController)
 const productRouter = require('./controllers/product.js')
 app.use('/product', productRouter)
 
+
 app.get('/about', (req, res) => {
     res.render('about.ejs', {
+        currentUser: null,
+    })
+});
+app.get('/', (req, res) => {
+    res.render('index.ejs', {
         currentUser: null,
     })
 });
